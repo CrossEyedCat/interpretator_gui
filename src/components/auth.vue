1c097866aa69f7cd395a6e1b12eb9c35803ffe49
ArtemZen <azenkevich@kvant-is.ru>
2023-10-31 21:26:56 +0300

    Добавил библиотеку naive-ui и реализовал авторизацию

--- src/components/auth.vue
+++ src/components/auth.vue
@@ -1,27 +1,46 @@
 <template>
-  <div>
-    <input type="text" placeholder="Логин" v-model="login">
-    <input type="password" placeholder="Пароль" v-model="password">
-    <button @click="loginUser">Вход</button>
-    <button @click="goToRegister">Регистрация</button>
+  <div class="login-container">
+    <n-input v-model="login" placeholder="Логин" />
+    <n-input v-model="password" placeholder="Пароль" type="password" />
+    <n-button @click="loginUser" type="primary">Вход</n-button>
+    <n-button @click="goToRegister" type="info">Регистрация</n-button>
   </div>
 </template>
 
 <script>
+import { NInput, NButton } from 'naive-ui';
+
 export default {
+  components: {
+    NInput,
+    NButton,
+  },
   data() {
     return {
       login: '',
-      password: ''
+      password: '',
     };
   },
   methods: {
     loginUser() {
-      // Logic for user login
+      // Логика входа пользователя
     },
     goToRegister() {
-      // Logic to go to registration page
-    }
-  }
-}
+      // Логика перехода на страницу регистрации
+    },
+  },
+};
 </script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: rgba(126, 120, 120, 0.99);
+}
+
+/* Дополнительные стили для компонентов можно добавить здесь */
+</style>
